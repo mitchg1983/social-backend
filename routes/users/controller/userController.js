d8f23e3b4edf2eb6374693357ac5ed30284cbd29
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2022-03-02 14:31:47 -0500

    Finish createUser, save data to mongoDB, validator & bcrypt working as advertised.

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -2,21 +2,33 @@ const User = require("../model/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+//Create a new user, POST method.
+//
+//
 const createUser = async (req, res, next) => {
   try {
-
     const { firstName, lastName, email, username, password } = req.body;
 
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
-    console.log(username);
-    console.log(password);
+    //Utilize bcrypt to hide the user password
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, salt);
 
-    let errObj = {};
+    //The 'User' is defined in the model
+    let newUser = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      username: username,
+      password: hashedPassword,
+    });
 
-    res.status(200).send({ message: "createUser beginning" });
+    let savedUser = await newUser.save();
+
+    res
+      .status(200)
+      .send({ message: "New user has been saved.", payload: savedUser });
   } catch (error) {
+    //errorHandler is used here in case of an error from mongoDB
     res.status(500).json({ error: errorHandler(error) });
   }
 };
--- routes/users/lib/authMiddleware/emptyCheck.js
+++ routes/users/lib/authMiddleware/emptyCheck.js
@@ -4,6 +4,9 @@ function emptyCheck(req, res, next) {
   let errObj = {};
   let body = req.body;
 
+  //Iterate through any & all keys in the req.body. 
+  //add a new key-value pair to the errObj, with the 'key' listed that is empty
+  //will not stop at one, will go through every key present
   for (let key in body) {
     if (isEmpty(body[key])) {
       errObj[key] = `${key} cannot be empty`;
@@ -12,6 +15,7 @@ function emptyCheck(req, res, next) {
 
   let checkObj = Object.keys(errObj);
 
+  //This will return a collection of every error collected in the body
   if (checkObj.length > 0) {
     return res.status(500).json({
       message: "Error inside emptyCheck",
